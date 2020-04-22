c36e1ce83a2cd7b8ae2b4d70e013b15d4833f4a0
maany <imptodefeat@gmail.com>
2020-04-23 01:33:49 +0200

    basic relay switching lamp

--- duinode/gardenator_duinode.ino
+++ duinode/gardenator_duinode.ino
@@ -1,8 +1,13 @@
+#define RELAY_LIGHT_PIN 5
 
 void setup(){
     Serial.begin(9600);
+    pinMode(RELAY_LIGHT_PIN, OUTPUT);
 }
 void loop(){
     Serial.println("Hello World");
     delay(1000);
+    digitalWrite(RELAY_LIGHT_PIN, HIGH);
+    delay(4000);
+    digitalWrite(RELAY_LIGHT_PIN, LOW);
 }
