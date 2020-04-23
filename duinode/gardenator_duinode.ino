6dc7c0225d951a045544be0b588cda29d0cf70b4
maany <imptodefeat@gmail.com>
2020-04-23 02:16:55 +0200

    LDR controlled relay

--- duinode/gardenator_duinode.ino
+++ duinode/gardenator_duinode.ino
@@ -1,13 +1,23 @@
 #define RELAY_LIGHT_PIN 5
-
+const int LDR_PIN = A0;
+const int LDR_CUTOFF = 200;
 void setup(){
     Serial.begin(9600);
     pinMode(RELAY_LIGHT_PIN, OUTPUT);
+    pinMode(LDR_PIN, INPUT);
 }
 void loop(){
-    Serial.println("Hello World");
-    delay(1000);
-    digitalWrite(RELAY_LIGHT_PIN, HIGH);
-    delay(4000);
-    digitalWrite(RELAY_LIGHT_PIN, LOW);
+    int ldr_value = analogRead(LDR_PIN);
+    
+    // Print LDR Value
+    Serial.print("LDR Value: ");
+    Serial.println(ldr_value);
+    
+    // Turn Light on if light is too little
+    // TODO add constaint on total time light has been present i.e light:dark ratio
+    if(ldr_value <= LDR_CUTOFF){
+        digitalWrite(RELAY_LIGHT_PIN, HIGH);
+    } else {
+        digitalWrite(RELAY_LIGHT_PIN, LOW);
+    }
 }
